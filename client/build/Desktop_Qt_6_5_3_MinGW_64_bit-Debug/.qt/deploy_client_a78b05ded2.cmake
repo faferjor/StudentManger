9de78ea13587021c83f1fa3b504f2c711f1f0af0
faferjor <hello-world-2005@foxmail.com>
2026-06-14 13:15:09 +0800

    Delete client directory

--- client/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/.qt/deploy_client_a78b05ded2.cmake
+++
@@ -1,8 +0,0 @@
-include(C:/Users/fafer/Desktop/studentManager/client/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/client-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_ALL_MODULES_FOUND_VIA_FIND_PACKAGE "ZlibPrivate;EntryPointPrivate;Core;Gui;Widgets")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/fafer/Desktop/studentManager/client/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/client.exe
-    GENERATE_QT_CONF
-)

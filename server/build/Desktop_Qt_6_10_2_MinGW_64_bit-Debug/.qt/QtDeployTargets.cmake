d4d6632ba11a58d4d50fdb702e5e76e39447abe8
faferjor <hello-world-2005@foxmail.com>
2026-06-14 13:14:57 +0800

    Delete server directory

--- server/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_StudentTopicSystemServer_FILE E:/code/Git/StudentManger/server/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/StudentTopicSystemServer.exe)
-set(__QT_DEPLOY_TARGET_StudentTopicSystemServer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_StudentTopicSystemServer_RUNTIME_DLLS E:/QT6.10/6.10.2/mingw_64/bin/Qt6Network.dll;E:/QT6.10/6.10.2/mingw_64/bin/Qt6Sql.dll;E:/QT6.10/6.10.2/mingw_64/bin/Qt6Core.dll)
